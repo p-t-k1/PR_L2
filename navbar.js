17677515be4e4087d47e8625fa750ba19f1590ed
Patryk Przybyło <patryk13342@o2.pl>
2021-03-25 08:30:16 +0100

    zadanie 1

--- navbar.js
+++ navbar.js
@@ -3,3 +3,17 @@ let nav = '<nav><a href="index.html">Home </a>' + '<a href="kontakt.html">Kontak
 let elem = document.querySelector(".navbar");
 elem.innerHTML=nav;
 
+let button = document.querySelector('.scroll');
+button.addEventListener('click', goToTop);
+function goToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+function showTopButton() {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        document.querySelector(".scroll").style.display = "block";
+    } else {
+        document.querySelector(".scroll").style.display = "none";
+    }
+}
+window.onscroll = function() {showTopButton()};
